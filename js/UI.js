337c9951021906d3170cba18a90a274350b74b33
Adebayo Adeyeye Godwin <adebayoadeyeye889@gmail.com>
2024-05-21 12:53:18 +0100

    adding my scripts functions

---
+++ js/UI.js
@@ -0,0 +1,89 @@
+let tickets = new Tickets();
+
+// Function for displaying contents
+function displayContents(tickets){
+    let htmlElement = $("#tickets-display");
+    let htmlString = "";
+    let keys = Object.keys(tickets.customers);
+    
+    keys.forEach(function(key){
+        let customer = tickets.findCustomer(key);
+        htmlString += "<li class = 'data' id = " + customer.id + ">" + customer.name + "</li>";
+    })
+    htmlElement.html(htmlString);
+}
+
+    // Create the function for attachng the listener
+function attachListener(){
+    $("ul#tickets-display").on("click" , "li" , function(){
+        displayDetails(this.id)
+    })
+
+    $(".display").on("click" , ".deleteButton" , function(){
+        tickets.deleteCustomer(this.id);
+        $(".display").hide();
+        displayContents(tickets);
+    })
+}
+
+// Function For Displaying The Full Details
+function displayDetails(id){
+    let customer = tickets.findCustomer(id);
+    $(".display").toggle();
+    $("#name-display").html(customer.name);
+    $("#age-display").html(customer.age);
+    $("#movie-display").html(customer.movie);
+    $("#price-display").html(customer.price);
+    let button = $("#delete-btn-div");
+    button.empty();
+    button.append("<button class = 'btn btn-light deleteButton' id = " + id + "> Delete </button>")
+    
+}
+
+$("document").ready(function(){
+    attachListener();
+
+    // The generate button click button
+    $("#generate").click(function(){
+        let name = $("#name-input").val();
+        let age = parseInt($("#age-input").val());
+        let movie = $("#movie-input").find(":selected").val();
+        let time = $("#time-input").find(":selected").val();
+
+        let customer = new Customer(name , age , time , movie);
+        let price = tickets.getPrice(customer);
+        $("#price-input").text(price)
+    });
+
+    // The Add btn click method
+    $("#add-btn").click(function(){
+        $(".price-sys").show();
+        $(".tickets").hide();
+        $(".form-input").toggle();
+    });
+
+
+    // Form Submission Method
+    $("#form").submit(function(e){
+        e.preventDefault();
+        let name = $("#name-input").val();
+        let age = $("#age-input").val();
+        let movie = $("#movie-input").find(":selected").val();
+        let time = $("#time-input").find(":selected").val();
+        let price = $("#price-input").text();
+
+        let customer = new Customer(name , age , time , movie , price);
+        tickets.addCustomer(customer);
+        displayContents(tickets)
+        $("#name-input").val("");
+        $("#age-input").val("");
+        $("#price-input").text("0");
+        $(".form-input").hide();
+    })
+
+    $("#tick-btn").click(function(){
+        $(".form-input").hide();
+        $(".tickets").toggle();
+        $(".price-sys").toggle();
+    })
+})
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,72 @@
-function sayHi(){
-    alert("Hello from MOVIE-TICKET");
+// Constructor For The Tickets
+function Tickets(){
+    this.customers = {
+
+    };
+    this.movies = {
+        "invincible" : 3000,
+        "despicable me4" : 3000,
+        "garfield" : 3000,
+        "kxgnewempire" : 3000,
+        "if" : 3000
+    };
+    this.id = 0;
 }
+
+
+// Method For Assigning Id
+Tickets.prototype.assignId = function(){
+    this.id += 1;
+    return this.id ;
+}
+
+// Method For Adding Customer
+Tickets.prototype.addCustomer = function(customer){
+    customer.id = this.assignId();
+    this.customers[customer.id] = customer;
+};
+
+// Method For Getting Pricing
+Tickets.prototype.getPrice = function(customer){
+    let price = this.movies[customer.movie];
+    if(price === undefined){
+        price = 0;
+    }
+    if(customer.age > 65){
+        price -= 500;
+    }
+    if(this.id > 3){
+        price -= 200 
+    }
+    if(customer.time ==="12:30 pm" || customer.time === "3:30 pm"){
+        price -= 200
+    }
+    
+    return price;
+};
+
+// Method For Finding Customer
+Tickets.prototype.findCustomer = function(id){
+    if(this.customers[id] !== undefined ){
+        return this.customers[id];
+    }
+    return false
+}
+
+// Method For Deleting Customerw
+Tickets.prototype.deleteCustomer = function(id){
+    if(this.customers[id] === undefined){
+        return false;
+    }
+    delete this.customers[id];
+    return true;
+}
+
+// Constructor For The Customer
+function Customer(name , age , time , movie , price){
+    this.name = name;
+    this.age = age;
+    this.time = time;
+    this.movie = movie;
+    this.price = price;
+};
